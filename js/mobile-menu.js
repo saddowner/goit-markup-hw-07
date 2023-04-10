12d6662303d7701405fe53936656744a52006ca8
saddowner <saddowner2@gmail.com>
2023-04-10 16:29:09 +0200

    final commit

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-open");
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
   }
 })();
